3d1fc5e9ffb6f3726eec28fc2d596480996d4ca3
Teddy Radenkov <teddy.pdx@gmail.com>
2017-02-19 20:34:40 -0800

    Stash

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ const {app, BrowserWindow} = require('electron');
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({width: 1200, height: 800, frame: false, backgroundColor: '#292929', thickFrame: false, focusable: true});
+  mainWindow = new BrowserWindow({width: 1200, height: 800, frame: false, backgroundColor: '#0b1015', thickFrame: false, focusable: true});
   mainWindow.setResizable(true);
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   mainWindow.openDevTools();
