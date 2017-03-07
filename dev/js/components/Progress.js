12847c444f8a8a2893e9ee28dbf2a403b7a8d9aa
Teddy Radenkov <teddy.pdx@gmail.com>
2017-03-06 21:24:52 -0800

    Added progress indicator html and css mockup

--- dev/js/components/Progress.js
+++ dev/js/components/Progress.js
@@ -2,13 +2,27 @@ import React from 'react';
 import Step from './Step'
 
 export default class Progress extends React.Component{
+  constructor(){
+    super();
+  }
+  next(){
+
+  }
+  reset(){
+
+  }
   render(){
     return(
       <div id="progress">
-        <Step label="Browse"/>
-        <Step label="Configure"/>
-        <Step label="Options"/>
-        <Step label="Finalize"/>
+        <div id="steps">
+          <Step label="Browse" status="complete" icon="fa fa-search"/>
+          <Step label="Configure" status="active" icon="fa fa-wrench"/>
+          <Step label="Options" status="await" icon="fa fa-cog"/>
+          <Step label="Finalize" status="await" icon="fa fa-check"/>
+        </div>
+        <div id="bar">
+          <div id="indicator"></div>
+        </div>
       </div>
     );
   }
--- dev/js/components/Step.js
+++ dev/js/components/Step.js
@@ -3,8 +3,17 @@ import React from 'react';
 export default class Step extends React.Component{
   render(){
     return(
-      <div id="step">
-
+      <div class="step">
+        <div class="icon">
+          <div class={this.props.status === 'await' ? 'await show' : 'await hide'}></div>
+          <div class={this.props.status === 'active' ? 'active show' : 'active hide'}>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+          </div>
+          <div class={this.props.status === 'complete' ? 'complete show' : 'complete hide'}><i class={this.props.icon} aria-hidden="true"></i></div>
+        </div>
+        <div class={'label ' + this.props.status}>{this.props.label}</div>
       </div>
     );
   }
