4bf105179419b104a9f78e7391d6d98bf5f0ba29
Teddy Radenkov <teddy.pdx@gmail.com>
2018-03-29 20:12:35 -0700

    Implemented options component

---
+++ src/modals/configure/components/options.jsx
@@ -0,0 +1,15 @@
+//React modules
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Options = () => {
+	return (
+		<div id='configure-options'>
+			<div id='configure-options-name'>Attribute Options</div>
+		</div>
+	);
+};
+
+Options.propTypes = {};
+
+export default Options;
