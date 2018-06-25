ffc3baca241024e104450d0e85abb804a635b654
Teddy Radenkov <teddy.pdx@gmail.com>
2018-06-24 18:24:38 -0700

    Fix spelling error

--- src/screens/rename/views/browse/components/messages.jsx
+++ src/screens/rename/views/browse/components/messages.jsx
@@ -6,7 +6,7 @@ const Messages = ({ count, handleBrowse, stage }) => {
 	return (
 		<div id='browse-messages'>
 			<h1 class={`message ${stage === 'browse' ? 'active' : 'inactive'}`}>
-				Browse all the files that you would like to add to be renamed.
+				Browse the files that you would like to add to be renamed.
 			</h1>
 			<h1 class={`message ${stage === 'loading' ? 'active' : 'inactive'}`}>
 				Please wait while we prepare your files to be configured.
