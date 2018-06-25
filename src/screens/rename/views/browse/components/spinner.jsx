c35e89e5f4082c4736bd41577c1c7d0eb5aa6c67
Teddy Radenkov <teddy.pdx@gmail.com>
2018-06-24 18:25:19 -0700

    Allow spinner to be responsive

--- src/screens/rename/views/browse/components/spinner.jsx
+++ src/screens/rename/views/browse/components/spinner.jsx
@@ -1,19 +1,31 @@
 //React modules
+import MediaQuery from 'react-responsive';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 const Spinner = ({ isSpinning }) => {
 	return (
-		<svg class={`spinner ${isSpinning ? 'active' : ''}`} width='370' height='370' viewport='0 0 100 100'>
-			<circle class='background' r='155' cx='185' cy='185' fill='transparent' strokeDasharray='973.89' />
+		<svg class={`spinner ${isSpinning ? 'active' : ''}`} viewport='0 0 100 100'>
+			<circle class='background small' r='155' cx='185' cy='185' fill='transparent' strokeDasharray='973.89' />
 			<circle
-				class={`loader ${isSpinning ? 'active' : ''}`}
+				class={`loader small ${isSpinning ? 'active' : ''}`}
 				r='155'
 				cx='185'
 				cy='185'
 				fill='transparent'
 				strokeDasharray='973.89'
 			/>
+			<MediaQuery query='(min-width: 1600px) and (min-height: 880px)'>
+				<circle class='background' r='180' cx='215' cy='215' fill='transparent' strokeDasharray='1130.97' />
+				<circle
+					class={`loader ${isSpinning ? 'active' : ''}`}
+					r='180'
+					cx='215'
+					cy='215'
+					fill='transparent'
+					strokeDasharray='1130.97'
+				/>
+			</MediaQuery>
 		</svg>
 	);
 };
