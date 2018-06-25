e63855630bc0e147d3bae31cb102f62e72a03be8
Teddy Radenkov <teddy.pdx@gmail.com>
2018-06-24 18:19:38 -0700

    Add option to build with Windows native title bar

---
+++ src/shared/navigation_win32/components/title.jsx
@@ -0,0 +1,13 @@
+//React modules
+import React from 'react';
+
+const Title = () => {
+	return (
+		<div id='title'>
+			<div id='logo' />
+			<div id='status'>Reflow</div>
+		</div>
+	);
+};
+
+export default Title;
---
+++ src/shared/navigation_win32/navigation.jsx
@@ -0,0 +1,24 @@
+//React modules
+import PropTypes from 'prop-types';
+import React from 'react';
+
+//Components
+import Controls from './components/controls.jsx';
+import Menu from './components/menu.jsx';
+import Title from './components/title.jsx';
+
+const Header = ({ history }) => {
+	return (
+		<div id='header-win32'>
+			<Title />
+			<Menu history={history} />
+			<Controls />
+		</div>
+	);
+};
+
+Header.propTypes = {
+	history: PropTypes.object.isRequired,
+};
+
+export default Header;
