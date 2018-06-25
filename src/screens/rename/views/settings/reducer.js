695b800f7172b7c97578da34ab60de9e69e905e9
Teddy Radenkov <teddy.pdx@gmail.com>
2018-06-24 22:02:34 -0700

    Rename index of given setting to active and add location cases

--- src/screens/rename/views/settings/reducer.js
+++ src/screens/rename/views/settings/reducer.js
@@ -1,25 +1,34 @@
 const defaultState = {
-	exists: '',
-	existsPath: '',
-	existsIndex: 0,
 	backup: '',
+	backupActive: 0,
 	backupPath: '',
-	backupIndex: 0,
+	exists: '',
+	existsActive: 0,
+	existsPath: '',
+	location: '',
+	locationActive: 0,
+	locationPath: '',
 };
 
 export default (state = defaultState, action) => {
 	switch (action.type) {
 	case 'SETTINGS_SET_BACKUP':
-		return { ...state, backup: action.payload.backup, backupIndex: action.payload.index };
+		return { ...state, backup: action.payload.backup, backupActive: action.payload.active };
 
 	case 'SETTINGS_SET_BACKUP_PATH':
 		return { ...state, backupPath: action.payload };
 
 	case 'SETTINGS_SET_EXISTS':
-		return { ...state, exists: action.payload.exists, existsIndex: action.payload.index };
+		return { ...state, exists: action.payload.exists, existsActive: action.payload.active };
 
 	case 'SETTINGS_SET_EXISTS_PATH':
 		return { ...state, existsPath: action.payload };
+
+	case 'SETTINGS_SET_LOCATION':
+		return { ...state, location: action.payload.location, locationActive: action.payload.active };
+
+	case 'SETTINGS_SET_LOCATION_PATH':
+		return { ...state, locationPath: action.payload };
 
 	case 'SETTINGS_CLEAR_DATA':
 		return defaultState;
