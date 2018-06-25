9afeaddc153db554ed441e0e35c71dcaa42abdd8
Teddy Radenkov <teddy.pdx@gmail.com>
2018-06-24 20:54:27 -0700

    Add reducer case to update attributes' ids after re-order

--- src/screens/rename/views/configure/containers/attributes/reducer.js
+++ src/screens/rename/views/configure/containers/attributes/reducer.js
@@ -1,6 +1,7 @@
 const defaultState = {
-	list: [],
+	data: [],
 	count: 0,
+	activeId: 0,
 	isBeingDragged: false,
 	hasBeenDropped: false,
 	scrollPosition: 0,
@@ -8,24 +9,45 @@ const defaultState = {
 
 export default (state = defaultState, action) => {
 	switch (action.type) {
-	case 'ATTRIBUTE_ADD':
+	case 'ATTRIBUTE_DATA_ADD':
 		return {
 			...state,
-			list: [...state.list, { ...action.payload, key: state.count }],
+			data: [...state.data, { ...action.payload, id: state.count }],
+			activeId: state.count,
 			count: state.count + 1,
-			scrollPosition: state.scrollPosition + 1,
+			scrollPosition: state.count + 1,
 		};
 
-	case 'ATTRIBUTE_DELETE':
+	case 'ATTRIBUTE_DATA_DELETE':
 		return {
 			...state,
-			list: state.list.filter(attribute => attribute.key !== action.payload),
+			data: state.data.filter(attribute => attribute.id !== action.payload),
 			count: state.count > 0 ? state.count - 1 : 0,
 			scrollPosition: state.scrollPosition > 0 ? state.scrollPosition - 1 : 0,
 		};
 
-	case 'ATTRIBUTE_LIST_UPDATE':
-		return { ...state, list: action.payload };
+	case 'ATTRIBUTE_DATA_UPDATE_OPTIONS':
+		return {
+			...state,
+			data: state.data.map(attribute => {
+				if (action.payload.id === attribute.id) return { ...attribute, options: action.payload.options };
+				return { ...attribute };
+			}),
+		};
+
+	case 'ATTRIBUTE_DATA_UPDATE_IDS':
+		return {
+			...state,
+			data: state.data.map((attribute, index) => {
+				return { ...attribute, id: index };
+			}),
+		};
+
+	case 'ATTRIBUTE_DATA_UPDATE_ACTIVE_ID':
+		return { ...state, activeId: action.payload };
+
+	case 'ATTRIBUTE_DATA_UPDATE':
+		return { ...state, data: action.payload };
 
 	case 'ATTRIBUTE_IS_BEING_DRAGGED_STATE_UPDATE':
 		return { ...state, isBeingDragged: action.payload };
@@ -36,7 +58,7 @@ export default (state = defaultState, action) => {
 	case 'ATTRIBUTE_SCROLL_POSITION_UPDATE':
 		return { ...state, scrollPosition: action.payload };
 
-	case 'DEFAULT_STATE':
+	case 'ATTRIBUTES_CLEAR_DATA':
 		return defaultState;
 
 	default:
