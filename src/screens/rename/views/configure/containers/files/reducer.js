a480710d1149c8343c4490547050435ab4b77b98
Teddy Radenkov <teddy.pdx@gmail.com>
2018-06-24 20:58:21 -0700

    Change list prop to data

--- src/screens/rename/views/configure/containers/files/reducer.js
+++ src/screens/rename/views/configure/containers/files/reducer.js
@@ -1,26 +1,26 @@
 const defaultState = {
-	list: [],
+	data: [],
 	count: 0,
 	loader: {
 		isLoading: false,
 		message: 'Loading files...',
 	},
-	unsaved: false,
+	path: '',
 };
 
 export default (state = defaultState, action) => {
 	switch (action.type) {
-	case 'FILES_LIST_UPDATE':
-		return { ...state, list: action.payload };
+	case 'FILES_DATA_LOAD':
+		return { ...state, data: action.payload };
 
-	case 'FILES_LIST_SORT':
+	case 'FILES_DATA_SORT':
 		switch (action.payload.type) {
 		case 'boolean':
 			return {
 				...state,
-				list: state.list.sort((fileA, fileB) => {
-					let compareA = fileA[action.payload.field];
-					let compareB = fileB[action.payload.field];
+				data: state.data.sort((fileA, fileB) => {
+					const compareA = fileA[action.payload.field];
+					const compareB = fileB[action.payload.field];
 					if (compareA && !compareB) return action.payload.direction == 'down' ? -1 : 1;
 					if (!compareA && compareB) return action.payload.direction == 'down' ? 1 : -1;
 					return 0;
@@ -29,9 +29,9 @@ export default (state = defaultState, action) => {
 		case 'string':
 			return {
 				...state,
-				list: state.list.sort((fileA, fileB) => {
-					let compareA = fileA[action.payload.field].toLowerCase();
-					let compareB = fileB[action.payload.field].toLowerCase();
+				data: state.data.sort((fileA, fileB) => {
+					const compareA = fileA[action.payload.field].toLowerCase();
+					const compareB = fileB[action.payload.field].toLowerCase();
 					if (compareA > compareB) return action.payload.direction == 'down' ? 1 : -1;
 					if (compareA < compareB) return action.payload.direction == 'down' ? -1 : 1;
 					return 0;
@@ -41,38 +41,29 @@ export default (state = defaultState, action) => {
 			return state;
 		}
 
-	case 'FILE_DELETE':
+	case 'FILES_SET_PATH':
+		return { ...state, path: action.payload };
+
+	case 'FILE_DATA_DELETE':
 		return {
 			...state,
-			list: state.list.filter(file => file.Key !== action.payload),
+			data: state.data.filter(file => file.id !== action.payload),
 			count: state.count > 0 ? state.count - 1 : 0,
 		};
 
 	case 'FILE_IS_SELECTED_TOGGLE':
 		return {
 			...state,
-			list: state.list.map(file => (file.Key === action.payload ? { ...file, isSelected: !file.isSelected } : file)),
+			data: state.data.map(file => (file.id === action.payload ? { ...file, isSelected: !file.isSelected } : file)),
 		};
 
-	case 'FILES_COUNT_SET':
+	case 'FILES_COUNT_UPDATE':
 		return { ...state, count: action.payload };
 
 	case 'FILES_LOADER_UPDATE':
 		return { ...state, loader: { isLoading: action.payload.isLoading, message: action.payload.message } };
 
-	case 'FILES_SET_UNSAVED':
-		return {
-			...state,
-			unsaved: true,
-		};
-
-	case 'FILES_CLEAR_UNSAVED':
-		return {
-			...state,
-			unsaved: false,
-		};
-
-	case 'DEFAULT_STATE':
+	case 'FILES_CLEAR_DATA':
 		return defaultState;
 
 	default:
